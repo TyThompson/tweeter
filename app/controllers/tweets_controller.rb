dbf965e892580012a5ae1fac9e714fb148a8a852
tythompson <tythompson@gmail.com>
2016-06-21 15:45:08 -0400

    edit/delete isn't working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,8 +11,22 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(params.require(:tweet).permit(:title, :text))
-    @tweet.save
-    redirect_to @tweet
-end
+  @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ActiveRecord::Base
+  validates :title, presence: true
+  validates :text, presence: true, length: { maximum: 170 }
 end
